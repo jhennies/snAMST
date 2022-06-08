879998d2296099f8dfc56e7769beb800b9c3741e
jhennies <julianhennies@hotmail.de>
2022-06-08 16:27:03 +0200

    updated memory requirement

--- run_amst.py
+++ run_amst.py
@@ -2,7 +2,7 @@
 import os
 import snakemake
 from glob import glob
-from amst_utils.common.settings import set_run_info, set_params, get_params_fp, get_params, get_workdir
+from amst_utils.common.settings import set_run_info, set_params, get_params_fp, get_params
 
 
 def run_amst(
