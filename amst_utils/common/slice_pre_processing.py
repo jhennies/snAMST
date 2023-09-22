5c608601db163efc511264c3ecef67b793a7c848
jhennies <julianhennies@hotmail.de>
2023-09-22 12:03:39 +0200

    trying some median filter magic for xcorr

--- amst_utils/common/slice_pre_processing.py
+++ amst_utils/common/slice_pre_processing.py
@@ -1,8 +1,9 @@
 
 from vigra.filters import gaussianSmoothing
+from vigra.filters import discRankOrderFilter
 
 
-def preprocess_slice(image, thresh=None, sigma=1., mask_range=None):
+def preprocess_slice(image, thresh=None, sigma=1., mask_range=None, med_diff=0):
 
     if mask_range is not None:
         assert image.dtype == 'uint8', 'Only implemented for uint8 datasets (for now)'
@@ -18,6 +19,13 @@ def preprocess_slice(image, thresh=None, sigma=1., mask_range=None):
         if thresh[1] > 0:
             image[image > thresh[1]] = thresh[1]
 
+    med_diff = 10
+    if med_diff > 0:
+        assert image.dtype == 'uint8'
+        med_filt = discRankOrderFilter(image, med_diff, rank=0.5)
+        image = image.astype('float32') - med_filt.astype('float32')
+        image[image < 0] = 0
+        image = image.astype('uint8')
     if sigma > 0:
         dtype = image.dtype
         image = gaussianSmoothing(image.astype('float32'), sigma)
